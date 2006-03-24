0220c78922fbcd8ea65b9b312cbd1a0062fbb20f
chris <chris>
2006-03-24 19:01:32 +0000

    Verify that we can read and write files.

---
+++ perllib/tests/File.pm
@@ -0,0 +1,110 @@
+#!/usr/bin/perl
+#
+# File.pm:
+# Test that we can create a file in various useful places.
+#
+# Copyright (c) 2006 UK Citizens Online Democracy. All rights reserved.
+# Email: chris@mysociety.org; WWW: http://www.mysociety.org/
+#
+# $Id: File.pm,v 1.1 2006-03-24 19:01:32 chris Exp $
+#
+
+package File;
+
+use strict;
+
+use Errno;
+use IO::File;
+use Time::HiRes qw(time);
+
+my @dirs = qw(
+        /tmp
+        /root
+        /var/log
+        /home
+        /data1
+        /data2
+    );
+
+use constant TEST_FILE_SIZE => 65536;
+use constant MAX_WRITE_TIME => 2;
+
+sub test () {
+    foreach my $dir (@dirs) {
+        next unless (-d $dir);
+        my ($name, $f, $i);
+        for ($i = 0; $i < 10; ++$i) {
+            $name = sprintf('%s/.monitor-test.%d.%d.%08x', $dir, $$, time(), rand(0xfffffff));
+            $f = new IO::File($name, O_RDWR | O_CREAT | O_EXCL, 0600);
+            if (!$f && !$!{EEXIST}) {
+                print "$dir: unable to create file: $!\n";
+                goto end;
+            } elsif ($f) {
+                last;
+            }
+        }
+
+        if (!$f) {
+            print "$dir: $i attempts to create test file failed with EEXIST\n";
+            goto end;
+        }
+
+        # Generate random buffer, write it to disk, read it back and check it's
+        # the same.
+        my $buf = '';
+        my $size = TEST_FILE_SIZE;
+        while (length($buf) < $size) {
+            $buf .= pack('L', int(rand(0xffffffff)));
+        }
+
+        my $t1 = time();
+
+        my $off = 0;
+        while ($off < length($buf)) {
+            my $n = $f->syswrite($buf, length($buf) - $off, $off);
+            if (!defined($n)) {
+                print "$dir: write to test file (after $off / $size bytes): $!\n";
+                goto end;
+            }
+            $off += $n;
+        }
+
+        if (!$f->sync()) {
+            print "$dir: fsync on test file: $!\n";
+            goto end;
+        }
+
+        my $t2 = time();
+        if (($t2 - $t1) > MAX_WRITE_TIME) {
+            printf "%s: write+sync of %d bytes took %.2fs (> %.2fs)\n",
+                    $dir, $size, $t2 - $t1, MAX_WRITE_TIME;
+        }
+
+        $f->seek(0, SEEK_SET);
+
+        my $buf2 = '';
+        $off = 0;
+        while ($off < length($buf)) {
+            my $n = $f->sysread($buf2, length($buf) - $off, $off);
+            if (!defined($n)) {
+                print "$dir: read from test file (after $off / $size bytes) : $!\n";
+                goto end;
+            } elsif ($n == 0) {
+                print "$dir: premature EOF reading from test file (after $off / $size bytes)\n";
+                goto end;
+            }
+            $off += $n;
+        }
+
+        if ($buf ne $buf2) {
+            print "$dir: data read back from test file differ from data written\n";
+            goto end;
+        }
+        
+end:
+        $f->close() if ($f);
+        unlink($name) if ($name);
+    }
+}
+
+1;
