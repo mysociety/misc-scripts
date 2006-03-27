bb64dcd2a7c2f376870923c458cd9c0213c3b221
chris <chris>
2006-03-27 12:05:18 +0000

    Test authoritative DNS.

---
+++ perllib/tests/DNS.pm
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+#
+# DNS.pm:
+# Test that local DNS servers are working.
+#
+# Copyright (c) 2006 UK Citizens Online Democracy. All rights reserved.
+# Email: chris@mysociety.org; WWW: http://www.mysociety.org/
+#
+# $Id: DNS.pm,v 1.1 2006-03-27 12:05:18 chris Exp $
+#
+
+package DNS;
+
+use strict;
+
+use Net::DNS;
+
+my @dnsservers = qw(svcs.tea.ukcod.org.uk svcs.bitter.ukcod.org.uk);
+
+my @records = (
+        [qw(www.pledgebank.com.             A)],
+        [qw(ukcod.org.uk.                   MX)],
+        [qw(101.230.111.82.in-addr.arpa.    PTR)]
+    );
+
+sub test () {
+    foreach my $dnsserver (@dnsservers) {
+        my $dns = new Net::DNS::Resolver(
+                        nameservers => [$dnsserver],
+                        recurse => 0
+                    );
+        $dns->tcp_timeout(2);
+        $dns->udp_timeout(2);
+        foreach my $record (@records) {
+            my ($name, $type) = @$record;
+            my $r = $dns->search($name, $type);
+            # XXX we just check that we get *a* successful response -- we
+            # should verify that it's sane too, probably.
+            if (!$r) {
+                print "$dnsserver: $type $name: no response (timed out?)\n";
+            } elsif ($r->header()->rcode() ne 'NOERROR') {
+                print "$dnsserver: $type $name: failed; rcode = ", $r->header()->rcode(), "\n";
+            }
+        }
+    }
+}
+
+1;
