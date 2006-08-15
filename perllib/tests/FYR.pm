f4252c0f322f8bb04a94f91a30f5ccc80e00c838
chris <chris>
2006-08-15 16:49:15 +0000

    Various fixes. Must drop privileges before calling FYR config file. Correct sense of some tests. Add missing \n on messages. Possibly this should be part of the fyr/ tree rather than in monitoring?

--- perllib/tests/FYR.pm
+++ perllib/tests/FYR.pm
@@ -6,20 +6,39 @@
 # Copyright (c) 2006 UK Citizens Online Democracy. All rights reserved.
 # Email: chris@mysociety.org; WWW: http://www.mysociety.org/
 #
-# $Id: FYR.pm,v 1.1 2006-08-15 16:41:35 chris Exp $
+# $Id: FYR.pm,v 1.2 2006-08-15 16:49:15 chris Exp $
 #
 
 package FYR;
 
 use strict;
 
+use POSIX qw();
+
 use mySociety::Config;
-use mySociety::DBHandle;
+use mySociety::DBHandle qw(dbh);
 
 use constant FYR_CONF_DIR => '/data/vhost/www.writetothem.com/mysociety/fyr/conf';
 
 sub test () {
-    return if (!-d FYR_CONF_DIR);
+    return if (!-d FYR_CONF_DIR || !-e FYR_CONF_DIR . "/general");
+    
+    # Mustn't call set_file as root since that would allow an fyr->root
+    # compromise. So drop privileges.
+    my $fyr_uid = getpwnam('fyr');
+    my $fyr_gid = getpwnam('fyr');
+    if (!defined($fyr_uid) || !defined($fyr_gid)) {
+        print "no user/group fyr, even though config file exists\n";
+        return;
+    }
+    if (!POSIX::setgid($fyr_gid)) {
+        print "setgid($fyr_gid): $!\n";
+        return;
+    }
+    if (!POSIX::setuid($fyr_uid)) {
+        print "setuid($fyr_uid): $!\n";
+        return;
+    }
     mySociety::Config::set_file(FYR_CONF_DIR . "/general");
 
     mySociety::DBHandle::configure(
@@ -69,17 +88,17 @@ sub test () {
     # a message has been submitted within 20 minutes during peak hours or
     # within 2 hours during off hours.
     my $message_threshold = 1200;
-    $message_threshold = 7200 if ($time lt '09:00' || $hour gt '22:00');
+    $message_threshold = 7200 if ($time lt '09:00' || $time gt '22:00');
     
-    printf("last message was submitted %d minutes ago", int($last_message_age / 60))
+    printf("last message was submitted %d minutes ago\n", int($last_message_age / 60))
         if ($last_message_age > $message_threshold);
     
-    printf("last fax was sent %d minutes ago", int($last_fax_age / 60))
-        if ($time ge '08:20' && $hour le '20:00'
-            && $n_ready_faxes > 0 && $last_fax_age < 1200);
+    printf("last fax was sent %d minutes ago\n", int($last_fax_age / 60))
+        if ($time ge '08:20' && $time le '20:00'
+            && $n_ready_faxes > 0 && $last_fax_age > 1200);
 
-    printf("last email was sent %d minutes ago", int($last_email_age / 60))
-        if ($n_ready_emails > 0 && $last_email_age < 1200);
+    printf("last email was sent %d minutes ago\n", int($last_email_age / 60))
+        if ($n_ready_emails > 0 && $last_email_age > 1200);
 
     # XXX maybe ought to check for messages being confirmed.
 
