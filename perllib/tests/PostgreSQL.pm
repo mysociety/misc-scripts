0129fca9535e54d097a14e9e720a7724c23ccd90
francis <francis>
2009-06-22 10:52:47 +0000

    Segment different query lengths to check against.

--- perllib/tests/PostgreSQL.pm
+++ perllib/tests/PostgreSQL.pm
@@ -6,7 +6,7 @@
 # Copyright (c) 2006 UK Citizens Online Democracy. All rights reserved.
 # Email: chris@mysociety.org; WWW: http://www.mysociety.org/
 #
-# $Id: PostgreSQL.pm,v 1.15 2009-06-02 17:32:27 francis Exp $
+# $Id: PostgreSQL.pm,v 1.16 2009-06-22 10:52:47 francis Exp $
 #
 
 package PostgreSQL;
@@ -19,13 +19,29 @@ use DBI;
 my @postgresql_servers = qw(tea.int.ukcod.org.uk bitter.int.ukcod.org.uk steak.int.ukcod.org.uk cake.int.ukcod.org.uk sandwich.int.ukcod.org.uk pudding.int.ukcod.org.uk stilton.int.ukcod.org.uk);
 my $postgresql_port = 5433;
 
+sub check_old_queries($$$$$$) {
+    my ($dbh, $age, $exceptions, $postgresql_server, $postgresql_port, $user) = @_;
+
+    # Find active queries which are old
+    my $sth = $dbh->prepare("select datname, usename, current_query, query_start, procpid from pg_stat_activity where current_query not like '<IDLE>%' and query_start < now() - '$age'::interval order by query_start");
+    if ( !defined $sth ) {
+        print "Cannot prepare statement on $postgresql_server:$postgresql_port as $user: $DBI::errstr\n";
+        next;
+    }
+    $sth->execute;
+    while ( my ($datname, $usename, $current_query, $query_start, $procpid) = $sth->fetchrow()) {
+        # some exceptions for particular queries
+        next if $current_query =~ m/$exceptions/; # for large petitions this does take ages
+        print "PostgreSQL query taking more than $age; server:$postgresql_server; database:$datname; user:$usename; process: $procpid; query:$current_query\n";
+    }
+}
+
 sub test() {
     return if !mySociety::Config::get('RUN_EXTRA_SERVERS_TESTS');
 
     my $user = mySociety::Config::get('MONITOR_PSQL_USER');
     my $pass = mySociety::Config::get('MONITOR_PSQL_PASS');
     foreach my $postgresql_server (@postgresql_servers) {
-
         # Connect to database
         my $port = $postgresql_port;
         $port = 5432 if $postgresql_server eq 'bitter.int.ukcod.org.uk'; # bitter has old PG database
@@ -36,16 +52,12 @@ sub test() {
             next;
         } 
 
-        # Find active queries which are old
-        my $sth = $dbh->prepare("select datname, usename, current_query, query_start, procpid from pg_stat_activity where current_query not like '<IDLE>%' and query_start < now() - '30 minutes'::interval order by query_start");
-        if ( !defined $sth ) {
-            print "Cannot prepare statement on $postgresql_server:$postgresql_port as $user: $DBI::errstr\n";
-            next;
-        }
-        $sth->execute;
-        while ( my ($datname, $usename, $current_query, $query_start, $procpid) = $sth->fetchrow()) {
-            print "PostgreSQL query taking more than 30 minutes; server:$postgresql_server; database:$datname; user:$usename; process: $procpid; query:$current_query\n";
-        }
+        # Check for long running queries
+        # ... check most queries take less than 30 minutes, with some exceptions (petitions delete from signer)
+        check_old_queries($dbh, "30 minutes", "\$(delete from signer)", $postgresql_server, $postgresql_port, $user); 
+        # .. show anything more than 12 hours long
+        check_old_queries($dbh, "12 hours", "\$^", $postgresql_server, $postgresql_port, $user); 
+
         $dbh->disconnect;
     }
 }
